02632ccafdf1bf021329af69caeeff611f5ab445
sital <sital@LAPTOP-7TL53IPC>
2024-11-08 20:50:14 +0530

    t1 task done by Dev1

--- GitProj01/src/main/java/com/nt/main/App.java
+++ GitProj01/src/main/java/com/nt/main/App.java
@@ -4,7 +4,13 @@ package com.nt.main;
  * Hello world!
  */
 public class App {
+	
+	public int sum(int x,int y) {
+		return x+y;
+	}
+	
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+    	App app=new App();
+        System.out.println("Sum:"+app.sum(10, 20));
     }
 }
--- GitProj01/src/test/java/com/nt/main/AppTest.java
+++ GitProj01/src/test/java/com/nt/main/AppTest.java
@@ -1,5 +1,6 @@
 package com.nt.main;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -9,11 +10,27 @@ import org.junit.jupiter.api.Test;
  */
 public class AppTest {
 
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+	@Test
+    public void testWithPositiveValues() {
+    	App app=new App();
+    	int expected=300;
+    	int actual=app.sum(100,200);
+    	assertEquals(expected,actual);
+    }
+	
+	@Test
+    public void testWithNegativeValues() {
+    	App app=new App();
+    	int expected=-300;
+    	int actual=app.sum(-100,-200);
+    	assertEquals(expected,actual);
+    }
+	
+	@Test
+    public void testWithMixedValues() {
+    	App app=new App();
+    	int expected=100;
+    	int actual=app.sum(-100,200);
+    	assertEquals(expected,actual);
     }
 }
