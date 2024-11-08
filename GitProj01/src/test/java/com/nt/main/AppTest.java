8ce6a5ec11a3da9a7ed3f34666a7e48048987ee2
sital <sital@LAPTOP-7TL53IPC>
2024-11-08 20:56:37 +0530

    Improved task1---TL

--- GitProj01/src/test/java/com/nt/main/AppTest.java
+++ GitProj01/src/test/java/com/nt/main/AppTest.java
@@ -33,4 +33,12 @@ public class AppTest {
     	int actual=app.sum(-100,200);
     	assertEquals(expected,actual);
     }
+	
+	@Test
+    public void testWithZeros() {
+    	App app=new App();
+    	int expected=0;
+    	int actual=app.sum(0,0);
+    	assertEquals(expected,actual);
+    }
 }
